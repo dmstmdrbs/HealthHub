3c5462a326be09722867a82c2b9ea95e00096376
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-05-29 01:07:22 +0900

    Add edit user information dialog

--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -10,7 +10,6 @@
 							<div class="d-flex flex-no-wrap justify-space-between">
 								<div style="text-align:center;">
 									<v-card-title class="text-h5" v-text="item.header"></v-card-title>
-									<v-btn block depressed small text outlined>수정</v-btn>
 								</div>
 								<v-divider></v-divider>
 								<v-col v-for="(content,i) in item.contents" :key="i" >
@@ -19,25 +18,41 @@
 									<v-card-text v-text="content.text"></v-card-text>
 								</v-col>
 							</div>
+							
 						</v-card>
 					</v-col>
 				</v-row>
 			</v-container>
+			<v-row>
+        		<v-col>
+					<div>
+					<v-btn elevation="2" small block v-on:click ="showSetDialog()" color ="green">수정</v-btn>
+					<v-dialog max-width="300" v-model="setDialog">
+                            <Dialog header-title = "수정" @hide="hideSetDialog" @submit="submitSetDialog">
+                              
+                            </Dialog>
+                          </v-dialog>
+						  </div>
+        		</v-col>
+			</v-row>
 		</template>
+		
 		<NaviBar></NaviBar>
 	</div>
 </template>
 <script>
 import Header from '@/components/Header'
 import NaviBar from '@/components/NaviBar.vue'
-
+import Dialog from '@/components/Dialog.vue'
 	export default {
-		components: { 
+		components: {
 			Header, 
 			NaviBar 
 		},
 		data () {
 		return{			
+			setDialog:false, 
+			is_show:false,
 				items: [
 					{
 						header: '개인 정보',
@@ -99,7 +114,19 @@ import NaviBar from '@/components/NaviBar.vue'
 			}
 		},
 		methods: {
-
+			hideSetDialog(){
+        this.setDialog=false;
+      }, 
+	  submitSetDialog(){
+        this.hideSetDialog();
+          
+      },
+			showSetDialog(){
+        this.setDialog=true;
+      },
+			handle_toggle: function(){ 
+      this.is_show = !this.is_show;
+    },
 		}    
 	}
 </script>
