21190f8e16ff43fc0de5e511b65ae67b609f9e21
taehong0-0 <xoghd5885@ajou.ac.kr>
2021-06-01 01:15:45 +0900

    Modify recommend.js workoutHistory

--- src/recommend.js
+++ src/recommend.js
@@ -1,21 +1,20 @@
-export let recommendedList=[];
-let userInfo={
-                weight:0.0, 
-                height:0.0,
-                sex='', 
-                oneRM={squat:0,bench=0,dead=0},
-                weakness="",
-                proficiency="",
-                };
-let workoutHistoy=[
-        {
-          target: '하체',
-          list:[
-            {'바벨 백스쿼트',}
-            {'컨벤셔널 데드리프트',}
-            {'프론트 스쿼트',}
-            {'브이 스쿼트',}
-            {'스미스 런지',}
+export let recommendedList = [];
+let userInfo = {
+    weight: 0.0,
+    height: 0.0,
+    sex = '',
+    oneRM = { squat: 0, bench = 0, dead = 0 },
+    weakness = "",
+    proficiency = "",
+};
+let workoutHistoy = [{
+        target: '하체',
+        list: [
+            '바벨 백스쿼트',
+            '컨벤셔널 데드리프트',
+            '프론트 스쿼트',
+            '브이 스쿼트',
+            '스미스 런지',
             '불가리안 스쿼트',
             '레그 익스텐션',
             '레그 컬',
@@ -24,11 +23,11 @@ let workoutHistoy=[
             '핵 스쿼트',
             '굿모닝 스쿼트',
             '힙 쓰러스트'
-          ]
-        },
-        {
-          target: '가슴',
-          list:[
+        ]
+    },
+    {
+        target: '가슴',
+        list: [
             '벤치프레스',
             '인클라인 벤치프레스',
             '디클라인 벤치 프레스',
@@ -40,29 +39,28 @@ let workoutHistoy=[
             '팩 덱 플라이 머신',
             '케이블 플라이',
             '덤벨 풀 오버',
-            '푸쉬업',                
-          ]
-        },
-        {
-          target: '등',
-          list:[
-            '루마니안 데드리프트',
-            '랫 풀 다운',
-            '풀 업',
-            '원 암 덤벨 로우',
-            '바벨 로우',
-            '티 바 로우',
-            '케이블 롱 풀',
-            '와이드 풀 다운',
-            '스트레이트 암 풀 다운',
-            '프론트 로우',
-            '팬들레이 로우',
-          ],
-        }
-        ,
-        {
-          target: '어깨',
-          list:[
+            '푸쉬업',
+        ]
+    },
+    {
+        target: '등',
+        list: [
+            { name: '루마니안 데드리프트', difficulty: 3 },
+            { name: '랫 풀 다운', difficulty: 2 },
+            { name: '풀 업', difficulty: 2 },
+            { name: '원 암 덤벨 로우', difficulty: 2 },
+            { name: '바벨 로우', difficulty: 3 },
+            { name: '티 바 로우', difficulty: 3 },
+            { name: '케이블 롱 풀', difficulty: 3 },
+            { name: '와이드 풀 다운', difficulty: 3 },
+            { name: '스트레이트 암 풀 다운', difficulty: 3 },
+            { name: '프론트 로우', difficulty: 3 },
+            { name: '팬들레이 로우', difficulty: 4 }
+        ],
+    },
+    {
+        target: '어깨',
+        list: [
             '오버 헤드 프레스',
             '덤벨 숄더 프레스',
             '사이드 레터럴 레이즈',
@@ -73,11 +71,11 @@ let workoutHistoy=[
             '업라이트 로우',
             '케이블 레터럴 레이즈',
             '리버스 케이블 플라이'
-          ]
-        },
-        {
-          target: '이두',
-          list:[
+        ]
+    },
+    {
+        target: '이두',
+        list: [
             '덤벨 컬',
             '이지바 컬',
             '케이블 푸쉬 다운',
@@ -85,35 +83,34 @@ let workoutHistoy=[
             '케이블 컬',
             '라잉 트라이셉스 익스텐션',
             '해머 컬',
-          ],
-        }
-    ];
-let workouts=[
+        ],
+    }
+];
+let workouts = [
 
 ];
 
 
-export function recommend(value){
-    recommendedList=[
-        {
-        target : '등',
-        kinds:'랫풀',
-        sets:[{reps:10, weight:60}]
+export function recommend(value) {
+    recommendedList = [{
+            target: '등',
+            kinds: '랫풀',
+            sets: [{ reps: 10, weight: 60 }]
         },
         {
-            target : '등',
-            kinds:'랫풀',
-            sets:[{reps:10, weight:60}]
+            target: '등',
+            kinds: '랫풀',
+            sets: [{ reps: 10, weight: 60 }]
         },
         {
-            target : '등',
-            kinds:'랫풀',
-            sets:[{reps:10, weight:60}]
+            target: '등',
+            kinds: '랫풀',
+            sets: [{ reps: 10, weight: 60 }]
         },
         {
-            target : '등',
-            kinds:'랫풀',
-            sets:[{reps:10, weight:60}]
+            target: '등',
+            kinds: '랫풀',
+            sets: [{ reps: 10, weight: 60 }]
         }
     ];
     console.log(recommendedList);
