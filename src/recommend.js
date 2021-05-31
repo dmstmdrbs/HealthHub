793bad5bde8d7ef4f6d358750d4633c7b0c90d54
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-06-01 02:59:00 +0900

    Add nextMainTarget

--- src/recommend.js
+++ src/recommend.js
@@ -1,13 +1,211 @@
+import { MIN_SAFE_INTEGER, random } from 'core-js/core/number';
+
 export let recommendedList = [];
 let userInfo = {
-    weight: 0.0,
-    height: 0.0,
-    sex = '',
-    oneRM = { squat: 0, bench = 0, dead = 0 },
+    weight: 77,
+    height: 179,
+    sex = '남자',
+    oneRM = { squat: 130, bench = 100, dead = 150 },
     weakness = "",
     proficiency = "",
 };
 let workoutHistory = [
+    {
+        date:"2021-05-20",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
+    {
+        date:"2021-05-20",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
+    {
+        date:"2021-05-22",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
+    {
+        date:"2021-05-23",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
+    {
+        date:"2021-05-25",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
+    {
+        date:"2021-05-27",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
+    {
+        date:"2021-05-30",
+        mainTarget:"등",
+        workouts:[
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [
+                    { reps: 10, weight: 60 }
+                ]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+            {
+                target: '등',
+                kinds: '랫풀',
+                sets: [{ reps: 10, weight: 60 }]
+            },
+        ]
+    },
 ];
 let workouts = [
     {
@@ -24,7 +222,7 @@ let workouts = [
             { name: '레그 프레스', difficulty: 2 },
             { name: '이너 타이 머신', difficulty: 2 },
             { name: '핵 스쿼트', difficulty: 3 },
-            { name: '굿모닝 스쿼트', difficulty: 4 },
+            { name: '굿모닝 스쿼트', difficulty: 5 },
             { name: '힙 쓰러스트', difficulty: 3 },
         ]
     },
@@ -33,7 +231,7 @@ let workouts = [
         list: [
             { name: '벤치프레스', difficulty: 3 },
             { name: '인클라인 벤치프레스', difficulty: 3 }, 
-            { name: '디클라인 벤치 프레스', difficulty: 4 }, 
+            { name: '디클라인 벤치프레스', difficulty: 5 }, 
             { name: '덤벨 벤치프레스', difficulty: 3 }, 
             { name: '인클라인 덤벨 벤치프레스', difficulty: 3 }, 
             { name: '딥스', difficulty: 3 }, 
@@ -52,13 +250,13 @@ let workouts = [
             { name: '랫 풀 다운', difficulty: 2 },
             { name: '풀 업', difficulty: 2 },
             { name: '원 암 덤벨 로우', difficulty: 2 },
-            { name: '바벨 로우', difficulty: 2 },
-            { name: '티 바 로우', difficulty: 3 },
+            { name: '바벨 로우', difficulty: 3 },
+            { name: '티 바 로우', difficulty: 4 },
             { name: '케이블 롱 풀', difficulty: 3 },
             { name: '와이드 풀 다운', difficulty: 3 },
-            { name: '스트레이트 암 풀 다운', difficulty: 3 },
-            { name: '프론트 로우', difficulty: 3 },
-            { name: '팬들레이 로우', difficulty: 4 }
+            { name: '스트레이트 암 풀 다운', difficulty: 4 },
+            { name: '프론트 로우', difficulty: 4 },
+            { name: '팬들레이 로우', difficulty: 5 }
         ],
     },
     {
@@ -91,28 +289,145 @@ let workouts = [
 ];
 
 
-export function recommend(value) {
-    recommendedList = [{
-            target: '등',
-            kinds: '랫풀',
-            sets: [{ reps: 10, weight: 60 }]
-        },
-        {
-            target: '등',
-            kinds: '랫풀',
-            sets: [{ reps: 10, weight: 60 }]
-        },
-        {
-            target: '등',
-            kinds: '랫풀',
-            sets: [{ reps: 10, weight: 60 }]
-        },
-        {
-            target: '등',
-            kinds: '랫풀',
-            sets: [{ reps: 10, weight: 60 }]
+
+// function nextMainTarget(){
+//     let nextMain="등"
+//     //logic
+//     //최근 기록 봐야되고, 이전 메인 타겟확인,
+//     //case1: 최근 기록이 너무 오래전일때
+//     //case2:
+//     //logic
+//     return nextMain
+// }
+
+function getReps(){
+
+}
+function getWeight(){
+
+}
+function getSets(){
+    sets:[]
+    let reps=0;
+    let weight=0;
+    const max_set=5;
+    for(var i=0;i<max_set;i++){
+        reps=getReps();
+        weight =getWeight();
+        sets.append({reps,weight});
+    }
+    return sets;
+}
+function nextMainTarget(){
+    let nextMain="등"
+
+    function getFormatDate(date){
+        var year = date.getFullYear();
+        var month = (1+date.getMonth());
+        mont = month >= 10?month:'0'+month;
+        var day = date.getDate();
+        day = day >=10?day:'0'+day;
+        return year + '-'+ month+ '-' +day;
+    }
+    function lastWeek() {
+        var d = new Date();
+        var dayOfMonth = d.getDate();
+        d.setDate(dayOfMonth - 7);
+        return getFormatDate(d);
+    }
+    //최근 일주일 내에 운동을 했는지 체크
+    let today = getFormatDate(new Date());
+    let lastweek = lastWeek(new Date());
+    
+    const reversedHistory = workoutHistory.reverse();
+    let idx=0;
+    let count = 0;
+    let latest='';
+    let freq={
+        chest=0,
+        leg=0,
+        back=0
+    }
+    reversedHistory.forEach((history)=>{
+        if(idx>7 & count===0){
+            break;
         }
-    ];
-    console.log(recommendedList);
+        if(history.date<today & history.date > lastweek)
+        {
+            count++;
+            if(count===1){
+                latest = history.mainTarget;
+            }
+            if(history.mainTarget==="가슴"){
+                freq.chest++;
+            }else if(history.mainTarget==="등"){
+                freq.back++;
+            }else if(history.mainTarget==="하체"){
+                freq.leg++;
+            }
+        }
+        idx++;
+    });
+    //가슴->등->하체 순서
+    //가장 많이한 것 제외
+    //case 1: 최근 일주일 내에 운동을 하지 않았을 때
+    if(count===0){
+        //가슴 등 하체
+        if(userInfo.weakness==="가슴"||userInfo.weakness==="등"||userInfo.weakness==="하체"){
+            nextMain=userInfo.weakness
+        }else{
+            nextMain="가슴"
+        }
+    }
+    //case 2: 최근 일주일 내에 운동을 했을 때
+    else{
+        //최근 운동이 있을 때
+        //case 1: 빈도가 같을 때 -> case 1-1: 전날 했던 것 제외하고 다음 것 or 약점 부위
+        //case 2: 빈도가 다를 때 -> 최하 빈도 부위
+        if(freq.back===freq.chest && freq.back==freq.chest){
+            //빈도가 모두 같을 때
+            if(latest === userInfo.weakness){
+                if(latest==="가슴" & userInfo.weakness){
+                    if(userInfo.sex==="여자"){
+                        if(freq.chest - freq.back >2 | freq.chest - freq.leg){
+
+                        }
+                    }
+                    else{
+
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+
+    return nextMain;
+}
+function getWorkout(target, difficulty, proficiency){
+    //랜덤으로 숙련도와 그에 따른 적합한 운동 난이도를 따져서 운동을 뽑는다.
+    let workout={};
+
+    //--------- logic ----------
+
+
+    sets=getSets(target);
+
+    //--------- logic ----------
+    return workout;
+}
+function makeList(){
+    let maxNum
+    let nextMain=nextMainTarget();
+    let list =[];
+    //logic
+    
+    //return 만들어진 운동 리스트
+    return list;
+}
+export function recommend(value) {
+    recommendedList = makeList();
+    console.log(recommendeList);
     return recommendedList;
 }
