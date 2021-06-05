a8d87774d677e593d34be40a7f390cdc9e431d41
Hong <tjdqls1668@ajou.ac.kr>
2021-06-05 18:53:59 +0900

    fix backend

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,72 +2,127 @@
 
 const UserStorage = require("./UserStorage");
 
-class User{
-    constructor(body){
-        this.body = body;
-    }
+class User {
+  constructor(body) {
+    this.body = body;
+  }
 
-    async login(){
-        const client = this.body;
-        try {
-            const user = await UserStorage.getUserInfo(client.id);
-      
-            if (user) {
-              if (user.id === client.id && user.psword === client.psword) {
-                return { success: true };
-              }
-              return { success: false, msg: "비밀번호가 틀렸습니다." };
-            }
-            return { success: false, msg: "존재하지 않는 아이디입니다." };
-          } catch (err) {
-            return { success: false, msg: err };
-          }
-    }
+  async login() {
+    const client = this.body;
+    try {
+      const user = await UserStorage.getUserInfo(client.id);
 
-    async register(){
-        const client = this.body;
-        try{
-        const response = await UserStorage.save(client);
-        return response;
-        } catch (err){
-            return { success : false, msg : err};
+      if (user) {
+        if (user.id === client.id && user.psword === client.psword) {
+          return { success: true };
         }
+        return { success: false, msg: "비밀번호가 틀렸습니다." };
+      }
+      return { success: false, msg: "존재하지 않는 아이디입니다." };
+    } catch (err) {
+      return { success: false, msg: err };
     }
-
-    async saveExercise(){
-      const client = this.body;
-      try{
-      const response = await UserStorage.updateExercise(client);
-      client
-      response  
-      return response;
-      } catch (err){
-          return { success : false, msg : err};
-      }
   }
 
-    async getExercise(){
-      const client = this.body;
-      try{
+  async checkID() {
+    const client = this.body;
+    try {
+      const user = await UserStorage.checkID(client.id);
+
+      if (user) {
+        if (user.id === client.id) {
+          return { success: true };
+        }
+      }
+
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
+
+
+  async register() {
+    const client = this.body;
+    try {
+
+      const user = await UserStorage.getUserInfo(client.id);
+
+      if (user) {
+        if (user.id === client.id) {
+          return { success: false, msg: err };
+        }
+      }
+      const response = await UserStorage.save(client);
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
+
+  async saveExercise() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.updateExercise(client);
+      client
+      response
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
+
+  async addUserInfo() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.addUserInfo(client);
+      client
+      response
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
+
+  async getUserUID() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.readUserUID(client);
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
+
+  async getExercise() {
+    const client = this.body;
+    try {
       const response = await UserStorage.readExercise(client);
-      client
-      response  
       return response;
-      } catch (err){
-          return { success : false, msg : err};
-      }
+    } catch (err) {
+      return { success: false, msg: err };
+    }
   }
 
-    async updateExercise(){
-      const client = this.body;
-      try{
-      const response = await UserStorage.updateExercise(client);
+  async saveUserInfo() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.updateUserInfo(client);
       client
-      response  
+      response
       return response;
-      } catch (err){
-          return { success : false, msg : err};
-      }
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
+
+  async getUserInfo() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.readUserInfo(client);
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
   }
 }
 
--- backend/src/routes/home/home.ctrl.js
+++ backend/src/routes/home/home.ctrl.js
@@ -3,46 +3,71 @@
 const User = require("../../models/User");
 
 const output = {
-    home : (req,res) => {
+    home: (req, res) => {
         res.render("home/index");
     },
-    login : (req,res)=>{
+    login: (req, res) => {
         res.render("home/login");
     },
-    register: (req, res)=>{
+    register: (req, res) => {
         res.render("home/register");
     }
 }
 
 
 const process = {
-    login : async (req, res) => {
+    login: async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
         return res.json(response);
-        
+
     },
-    register : async (req, res) => {
+    checkID: async (req, res) => {
+        const user = new User(req.body);
+        const response = await user.checkID();
+        return res.json(response);
+
+    },
+    register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
         return res.json(response);
     },
 
-    saveExercise : async (req, res) => {
+    addUserInfo: async (req, res) => {
+        const user = new User(req.body);
+        const response = await user.addUserInfo();
+        return res.json(response);
+
+    },
+
+    getUserUID: async (req, res) => {
+        req.body = req.params.id;
+        const user = new User(req.body);
+        const response = await user.getUserUID();
+        return res.json(response);
+    },
+
+    saveExercise: async (req, res) => {
         const user = new User(req.body);
         const response = await user.saveExercise();
         return res.json(response);
     },
-    getExercise : async (req, res) => {
+    getExercise: async (req, res) => {
         req.body = req.params.date;
         const user = new User(req.body);
         const response = await user.getExercise();
         return res.json(response);
     },
-    updateExercise : async (req, res) => {
-        req.body = req.params.date;
+    saveUserInfo: async (req, res) => {
         const user = new User(req.body);
-        const response = await user.updateExercise();
+        const response = await user.saveUserInfo();
+        return res.json(response);
+    },
+    getUserInfo: async (req, res) => {
+        req.body = req.params.uName;
+        const user = new User(req.body);
+        const response = await user.getUserInfo();
         return res.json(response);
     },
 
