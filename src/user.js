97d07df821d01ff2ed8076669af6e83e1cb21694
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-06-06 15:56:29 +0900

    Add user objects

--- src/user.js
+++ src/user.js
@@ -1,5 +1,38 @@
-export let uid = 0;
-export let id = '';
-export let name = '';
-export let sex = 0;
-export let age = 0;
+import { userInfo } from '@/userInfo.js';
+
+export let user = {
+  uID: 123,
+  id: '',
+  userInfo: userInfo,
+};
+export function saveUser() {
+  const req = {
+    uName: this.user.userInfo.name,
+    age: parseInt(this.user.userInfo.age),
+    sex: this.user.userInfo.sex,
+    height: parseInt(this.user.userInfo.height),
+    weight: parseInt(this.user.userInfo.weight),
+    sqrt: parseInt(this.user.userInfo.sqrt),
+    bench: parseInt(this.user.userInfo.bench),
+    dead: parseInt(this.user.userInfo.dead),
+    weak: this.user.userInfo.weak,
+  };
+  fetch('http://115.85.183.157:3000/userInfo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(req),
+  })
+    .then(res => res.json())
+    .then(res => {
+      if (res.success) {
+        alert('성공');
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch(err => {
+      console.error('error');
+    });
+}
---
+++ src/userInfo.js
@@ -0,0 +1,12 @@
+export let userInfo = {
+  uName: 'default',
+  age: '24',
+  sex: '남자',
+  height: '179',
+  weight: '77',
+  squat: 130,
+  bench: 100,
+  dead: 150,
+  weak: '등',
+  proficiency: '중급자',
+};
