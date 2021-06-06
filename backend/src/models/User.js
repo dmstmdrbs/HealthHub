2b7b28aa23a7479340b8b07102f365a699c23da4
fbwlgh07 <fbwlgh07@gmail.com>
2021-06-07 00:15:16 +0900

    DB userInfo save and update

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,127 +2,140 @@
 
 const UserStorage = require("./UserStorage");
 
-class User {
-  constructor(body) {
-    this.body = body;
-  }
-
-  async login() {
-    const client = this.body;
-    try {
-      const user = await UserStorage.getUserInfo(client.id);
-
-      if (user) {
-        if (user.id === client.id && user.psword === client.psword) {
-          return { success: true };
-        }
-        return { success: false, msg: "비밀번호가 틀렸습니다." };
-      }
-      return { success: false, msg: "존재하지 않는 아이디입니다." };
-    } catch (err) {
-      return { success: false, msg: err };
+class User{
+    constructor(body){
+        this.body = body;
     }
-  }
 
-  async checkID() {
-    const client = this.body;
-    try {
-      const user = await UserStorage.checkID(client.id);
-
-      if (user) {
-        if (user.id === client.id) {
-          return { success: true };
-        }
-      }
-
-    } catch (err) {
-      return { success: false, msg: err };
+    async login(){
+        const client = this.body;
+        try {
+            const user = await UserStorage.getUserInfo(client.id);
+      
+            if (user) {
+              if (user.id === client.id && user.psword === client.psword) {
+                return { success: true };
+              }
+              return { success: false, msg: "비밀번호가 틀렸습니다." };
+            }
+            return { success: false, msg: "존재하지 않는 아이디입니다." };
+          } catch (err) {
+            return { success: false, msg: err };
+          }
     }
-  }
 
-
-  async register() {
-    const client = this.body;
-    try {
-
-      const user = await UserStorage.getUserInfo(client.id);
-
-      if (user) {
-        if (user.id === client.id) {
-          return { success: false, msg: err };
-        }
-      }
-      const response = await UserStorage.save(client);
-      return response;
-    } catch (err) {
-      return { success: false, msg: err };
+    async checkID(){
+        const client = this.body;
+        try {
+            const user = await UserStorage.checkID(client.id);
+      
+            if (user) {
+              if (user.id === client.id) {
+                return { success: true };
+              }
+            }
+            
+          } catch (err) {
+            return { success: false, msg: err };
+          }
     }
-  }
 
-  async saveExercise() {
-    const client = this.body;
-    try {
+
+    async register(){
+        const client = this.body;
+        try{
+
+          const user = await UserStorage.getUserInfo(client.id);
+      
+            if (user) {
+              if (user.id === client.id) {
+                return { success: false, msg:err};
+              }
+            }
+        const response = await UserStorage.save(client);
+        return response;
+        } catch (err){
+            return { success : false, msg : err};
+        }
+    }
+
+    async saveExercise(){
+      const client = this.body;
+      try{
       const response = await UserStorage.updateExercise(client);
       client
       response
       return response;
-    } catch (err) {
-      return { success: false, msg: err };
-    }
+      } catch (err){
+          return { success : false, msg : err};
+      }
   }
 
-  async addUserInfo() {
-    const client = this.body;
-    try {
+  async addUserInfo(){
+      const client = this.body;
+      try{
       const response = await UserStorage.addUserInfo(client);
       client
       response
       return response;
-    } catch (err) {
-      return { success: false, msg: err };
-    }
+      } catch (err){
+          return { success : false, msg : err};
+      }
   }
 
-  async getUserUID() {
-    const client = this.body;
-    try {
+  async getUserUID(){
+      const client = this.body;
+      try{
       const response = await UserStorage.readUserUID(client);
       return response;
-    } catch (err) {
-      return { success: false, msg: err };
-    }
+      } catch (err){
+          return { success : false, msg : err};
+      }
   }
 
-  async getExercise() {
-    const client = this.body;
-    try {
+    async getExercise(){
+      const client = this.body;
+      try{
       const response = await UserStorage.readExercise(client);
       return response;
-    } catch (err) {
-      return { success: false, msg: err };
-    }
+      } catch (err){
+          return { success : false, msg : err};
+      }
   }
 
-  async saveUserInfo() {
-    const client = this.body;
-    try {
+  async saveUserInfo(){
+      const client = this.body;
+      try{
       const response = await UserStorage.updateUserInfo(client);
       client
       response
       return response;
-    } catch (err) {
-      return { success: false, msg: err };
-    }
+      } catch (err){
+          return { success : false, msg : err};
+      }
   }
 
-  async getUserInfo() {
-    const client = this.body;
-    try {
+    async getUserInfo(){
+      const client = this.body;
+      try{
       const response = await UserStorage.getUserInfo(client);
       return response;
-    } catch (err) {
-      return { success: false, msg: err };
-    }
+      } catch (err){
+          return { success : false, msg : err};
+      }
+
+      
+  }
+  async getUserInfo_uID(){
+      const client = this.body;
+      try{
+      const response = await UserStorage.getUserInfo_uID(client);
+      return response;
+      } catch (err){
+          return { success : false, msg : err};
+      }
+
+      
   }
 }
 
--- backend/src/routes/home/index.js
+++ backend/src/routes/home/index.js
@@ -5,22 +5,25 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.output.home);
-router.get("/login", ctrl.output.login);
-router.get("/register", ctrl.output.register);
+router.get("/",ctrl.output.home);
+router.get("/login",ctrl.output.login);
+router.get("/register",ctrl.output.register);
 
-router.get("/exercises/:date", ctrl.process.getExercise);
-router.get("/userInfo/:uName", ctrl.process.getUserInfo);
-router.get("/register/userInfo/getUID/:id", ctrl.process.getUserUID);
+router.get("/exercises/:uID/:dates",ctrl.process.getExercise);
+router.get("/userInfo/:uName",ctrl.process.getUserInfo);
+router.get("/userInfo/getUserInfo/:uID",ctrl.process.getUserInfo_uID);
+router.get("/register/userInfo/getUID/:id",ctrl.process.getUserUID);
 
 
-router.post("/login", ctrl.process.login);
-router.post("/checkID", ctrl.process.checkID);
-router.post("/register", ctrl.process.register);
+
+
+router.post("/login",ctrl.process.login);
+router.post("/checkID",ctrl.process.checkID);
+router.post("/register",ctrl.process.register);
 
 router.post("/exercises", ctrl.process.saveExercise);
-router.post("/register/userInfo", ctrl.process.addUserInfo);
+router.post("/register/userInfo",ctrl.process.addUserInfo);
 
-router.put("/userInfo", ctrl.process.saveUserInfo);
+router.put("/userInfo",ctrl.process.saveUserInfo);
 
 module.exports = router;
