9ca6835767298adfcc7796aa21cd2168aa605d33
Seunggyun98 <dmstmdrbs@ajou.ac.kr>
2021-06-07 15:21:26 +0900

    Change output directory

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/HealthHub/' : '/',
+    outputDir: 'docs',
+    transpileDependencies: [
+        'vuetify'
+    ]
 }
